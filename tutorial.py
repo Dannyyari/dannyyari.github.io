1d7908427cf546cb627b1407704a505235cd7f4f
Dannyyari <160466425+Dannyyari@users.noreply.github.com>
2025-09-20 12:49:16 +0200

    first commit

---
+++ tutorial.py
@@ -0,0 +1,13 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello, main page of Flask! <h1>Welcome to Flask!</h1>"
+
+@app.route("/<name>")
+def user(name):
+    return f"Hello, {name}"
+
+if __name__ == "__main__":
+    app.run()
