a627788a6acfdc0e44b40b65a5b628dcfff9d189
Dannyyari <160466425+Dannyyari@users.noreply.github.com>
2025-09-20 12:50:16 +0200

    change of nmae

--- controller.py
+++ controller.py
@@ -6,8 +6,8 @@ def home():
     return "Hello, main page of Flask! <h1>Welcome to Flask!</h1>"
 
 @app.route("/<name>")
-def user(name):
-    return f"Hello, {name}"
+def user(name):  
+     return f"Hello, {name}"
 
 if __name__ == "__main__":
     app.run()
