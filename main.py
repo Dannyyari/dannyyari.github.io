dd6c480534fb00043747d46c1a88a337805ff3f3
Dannyyari <danny_yari@yahoo.com>
2025-09-21 17:43:03 +0200

    file reconstruction

--- main.py
+++ main.py
@@ -3,9 +3,28 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, main page of Flask! <h1>Welcome to Flask!</h1>"
+    return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/resume")
+def resume():
+    return render_template("resume.html")
 
 
+@app.route("/services")
+def services():
+    return render_template("services.html")
+
+@app.route("/portfolio")
+def portfolio():
+    return render_template("portfolio.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
