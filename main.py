ae37e52994d3f51e88a966032a2dcfb2359ed766
Dannyyari <danny_yari@yahoo.com>
2025-09-23 09:57:05 +0200

    Service and porfolio HTML working

--- main.py
+++ main.py
@@ -5,24 +5,29 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
-@app.route("/about")
+@app.route("/about.html")
 def about():
     return render_template("about.html")
 
-@app.route("/resume")
+@app.route("/resume.html")
 def resume():
     return render_template("resume.html")
 
 
-@app.route("/services")
+@app.route("/services.html")
 def services():
     return render_template("services.html")
 
-@app.route("/portfolio")
+@app.route("/portfolio.html")
 def portfolio():
     return render_template("portfolio.html")
 
-@app.route("/contact")
+@app.route("/portfolio/<int:project_id>")
+def portfolio_details(project_id):
+    # Här kan du t.ex. hämta information om projektet med project_id
+    return render_template("portfolio-details.html", project_id=project_id)
+
+@app.route("/contact.html")
 def contact():
     return render_template("contact.html")
 
