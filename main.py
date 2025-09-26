0604c243dd5f3ba3b59499ce38c5142430054763
Dannyyari <danny_yari@yahoo.com>
2025-09-26 09:55:04 +0200

    changing Main.py

--- main.py
+++ main.py
@@ -1,39 +1,5 @@
-from flask import Flask, url_for, render_template, request, jsonify
-app = Flask(__name__, static_folder="frontend/static",      # <-- pekar på din static
-    template_folder="frontend/templates"  # <-- pekar på dina templates
-    )
-
-@app.route("/")
-@app.route("/home")
-def home():
-    return render_template("index.html")
-
-@app.route("/about")
-def about():
-    return render_template("about.html")
-
-@app.route("/resume")
-def resume():
-    return render_template("resume.html")
-
-
-@app.route("/services")
-def services():
-    return render_template("services.html")
-
-@app.route("/portfolio")
-def portfolio():
-    return render_template("portfolio.html")
-
-@app.route("/portfolio/<int:project_id>")
-def portfolio_details(project_id):
-    # Här kan du t.ex. hämta information om projektet med project_id
-    return render_template("portfolio-details.html", project_id=project_id)
-
-@app.route("/contact")
-def contact():
-    return render_template("contact.html")
-
+from flask import Flask, request, jsonify
+app = Flask(__name__)
 
 @app.route("/api/contact", methods=["POST"])
 def contact_api():
